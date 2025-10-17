e5d29f35ecbe1b74913b68bbf0b062c6631e6044
LeeTaeYoung <xodud5080@naver.com>
2025-10-17 12:47:17 +0900

    Mapped Superclass

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -16,35 +18,15 @@ public class JpaMain {
         tx.begin();
 
         try{
+            Member member = new Member();
+            member.setUsername("user");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            Movie movie = new Movie();
-            movie.setDirector("aaa");
-            movie.setActor("bbb");
-            movie.setName("바람과 함께 사라지다");
-            movie.setPrice(10000);
-
-            em.persist(movie);
-
-            Album album = new Album();
-            album.setArtist("태풍");
-            album.setName("뛰어");
-            album.setPrice(20000);
-
-            em.persist(album);
-
-            Book book = new Book();
-            book.setAuthor("베르나르");
-            book.setIsbn("띠용");
-            book.setName("노인과 바다");
-            book.setPrice(30000);
-
-            em.persist(book);
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("find movie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
