d5de66c8a34b5393cdf32bcccda5a6a3302cccbe
LeeTaeYoung <xodud5080@naver.com>
2025-10-17 12:22:10 +0900

    Inheritance Mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,16 +16,35 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
 
-            Team team = new Team();
-            team.setName("teamA");
-            //
-            team.getMembers().add(member);
+            Movie movie = new Movie();
+            movie.setDirector("aaa");
+            movie.setActor("bbb");
+            movie.setName("바람과 함께 사라지다");
+            movie.setPrice(10000);
 
-            em.persist(team);
+            em.persist(movie);
+
+            Album album = new Album();
+            album.setArtist("태풍");
+            album.setName("뛰어");
+            album.setPrice(20000);
+
+            em.persist(album);
+
+            Book book = new Book();
+            book.setAuthor("베르나르");
+            book.setIsbn("띠용");
+            book.setName("노인과 바다");
+            book.setPrice(30000);
+
+            em.persist(book);
+
+            em.flush();
+            em.clear();
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("find movie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
