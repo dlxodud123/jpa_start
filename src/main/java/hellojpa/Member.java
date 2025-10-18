8dbd5e5b7e42318be41364cf58bbdd8e68174fb5
LeeTaeYoung <xodud5080@naver.com>
2025-10-18 10:18:21 +0900

    embedded type

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,7 +3,7 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-public class Member extends BaseEntity{
+public class Member{
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -11,35 +11,55 @@ public class Member extends BaseEntity{
 
     private String username;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "TEMA_ID")
-    private Team team;
+    // 기간(Period)
+    @Embedded
+    private Period workPeriod;
 
+    // 주소(Address)
+    @Embedded
+    private Address homeAddress;
 
+    // work주소
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city",
+                    column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",
+                    column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",
+                    column = @Column(name = "WORK_ZIPCODE")),
+    })
+    private Address workAddress;
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
-    public Team getTeam() {
-        return team;
+
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
-    public void setTeam(Team team) {
-        this.team = team;
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
     }
-//
-//
-//    // 연관관계 편의 메서드
-//    public void changeTeam(Team team) {
-//        this.team = team;
-//        team.getMembers().add(this);
-//    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
 }
